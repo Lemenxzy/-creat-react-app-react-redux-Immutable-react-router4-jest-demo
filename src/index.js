3f9ac607638f57ae63d60743b30b2fe6052c0ef5
许智源 <704694231@qq.com>
2017-12-20 14:03:50 +0800

    delete and browerrtouter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux';
 import store from './store';
 ReactDOM.render(
@@ -13,4 +12,3 @@ ReactDOM.render(
         </Router>
     </Provider>,
     document.getElementById('root'));
-registerServiceWorker();
